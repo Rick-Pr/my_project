8eff066295f01ab90b263f73395b228b35420dce
Rick-Pr <k9211471671@gmail.com>
2024-01-06 00:56:26 +0300

    6

--- src/vebtts/tts/tests.py
+++ src/vebtts/tts/tests.py
@@ -5,4 +5,4 @@ class TestSite(TestCase):
     def test_index_page(self):
         self.assertEquals(self.client.get('').status_code, 200)
     def test_admin_page(self):
-            self.assertEquals(self.client.get('admin/').status_code, 200)
+        self.assertEquals(self.client.get('admin/').status_code, 200)
