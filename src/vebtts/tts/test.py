800d3dc6df9df989eb020c22a68c34b6e89631b7
Rick-Pr <k9211471671@gmail.com>
2024-01-06 22:58:05 +0300

    6

--- src/vebtts/tts/test.py
+++ src/vebtts/tts/test.py
@@ -4,7 +4,9 @@ import pytest
 
 @pytest.fixture()
 class TestSite(TestCase):
+
     def test_index_page(self):
         self.assertEquals(self.client.get('').status_code, 200)
+
     def test_admin_page(self):
         self.assertEquals(self.client.get('admin/').status_code, 200)
